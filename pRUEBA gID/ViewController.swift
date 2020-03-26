eadc1ed554a30d69d959bee4f731651ec490841e
mauricio cervera <maucervera16@yahoo.com.mx>
2020-03-26 01:31:33 -0600

    Initial Commit

---
+++ pRUEBA gID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pRUEBA gID
+//
+//  Created by Apple on 3/26/20.
+//  Copyright © 2020 bgddddtec de monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pRUEBA gIDTests/pRUEBA_gIDTests.swift
@@ -0,0 +1,36 @@
+//
+//  pRUEBA_gIDTests.swift
+//  pRUEBA gIDTests
+//
+//  Created by Apple on 3/26/20.
+//  Copyright © 2020 bgddddtec de monterrey. All rights reserved.
+//
+
+import XCTest
+@testable import pRUEBA_gID
+
+class pRUEBA_gIDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
